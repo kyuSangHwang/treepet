44ac137b9cf585cb833706ccca4481f4aa25fd36
YooSangWoo <qswwsq11@gmail.com>
2023-01-30 20:40:51 +0900

    텍스트 필드

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:treepet/const/color.dart';
+import 'package:treepet/const/style.dart';
 
 class CustomTextField extends StatelessWidget {
   // true : 제목 ,  false : 내용
@@ -23,18 +24,22 @@ class CustomTextField extends StatelessWidget {
   }
 
   Widget renderTextField() {
-    return TextFormField(
-      cursorColor: SECOND_COLOR,
-      style: TextStyle(fontSize: 20.0),
-      maxLines: isTitle ? 1 : null,
-      minLines: isTitle ? 1 : 5,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        hintText: placeHolder,
-        suffixText: isTitle ? '20자 이내' : '300자 이내' ,
-        suffixStyle: TextStyle(
-          fontSize: 12.0,
-        )
+    return Padding(
+      padding: post_item_left_padding(),
+      child: TextFormField(
+        cursorColor: SECOND_COLOR,
+        style: co_create_textfield_placeholder,
+        maxLines: isTitle ? 1 : null,
+        minLines: isTitle ? 1 : 12,
+        decoration: InputDecoration(
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          hintText: placeHolder,
+          suffixText: isTitle ? '20자 이내' : '1000자 이내' ,
+          suffixStyle: TextStyle(
+            fontSize: 12.0,
+          )
+        ),
       ),
     );
   }
