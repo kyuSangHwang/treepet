51f7e4ff9b90a7207af87ab420e67082b4cd47ad
DopeKS <kyusang.dev@gmail.com>
2023-02-19 22:54:08 +0900

    Calender Component 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:treepet/screen/onboarding/onboarding_page.dart';
 import 'firebase_options.dart';
-import 'package:treepet/screen/home_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
